f71117e10969462bfca2126e9fe2d7532ea4e899
node <pillisaiganesh79@gmail.com>
2025-07-08 07:53:08 +0530

    changed path to live in api_url

--- dashboard/src/vendorDashboard/data/apiPath.js
+++ dashboard/src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,2 @@
-export const API_URL = "http://localhost:4000"
-
-//export const API_URL = "https://backend-nodejs-ruby-r6ra.onrender.com"
+//export const API_URL = "http://localhost:4000"
+export const API_URL = "https://backend-nodejs-ruby-r6ra.onrender.com"
